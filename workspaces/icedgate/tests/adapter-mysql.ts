3f19f7b7eb872baafbf35e07eb334c1e3a1b4839
Jumpei Ogawa <git@phanective.org>
2024-11-01 06:53:57 +0900

    refactor: lint

--- workspaces/icedgate/tests/adapter-mysql.ts
+++ workspaces/icedgate/tests/adapter-mysql.ts
@@ -1,22 +1,22 @@
-import { testAdapter, databaseUser } from "@lucia-auth/adapter-test";
-import { DrizzleMySQLAdapter } from "../src/drivers/mysql.js";
+import { resolve } from "node:path";
 import mysql from "mysql2/promise";
 
 import dotenv from "dotenv";
-import { resolve } from "path";
+import { testAdapter, databaseUser } from "@lucia-auth/adapter-test";
 
 import { mysqlTable, varchar, datetime } from "drizzle-orm/mysql-core";
 import { drizzle } from "drizzle-orm/mysql2";
+import { DrizzleMySQLAdapter } from "../src/drivers/mysql.js";
 
 dotenv.config({
-	path: resolve(".env")
+  path: resolve(".env"),
 });
 
 const connection = await mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	database: process.env.MYSQL_DATABASE,
-	password: process.env.MYSQL_PASSWORD
+  host: "localhost",
+  user: "root",
+  database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_PASSWORD,
 });
 
 await connection.execute("DROP TABLE IF EXISTS user_session");
@@ -36,34 +36,34 @@ await connection.execute(`CREATE TABLE IF NOT EXISTS user_session (
 )`);
 
 await connection.execute("INSERT INTO test_user (id, username) VALUES (?, ?)", [
-	databaseUser.id,
-	databaseUser.attributes.username
+  databaseUser.id,
+  databaseUser.attributes.username,
 ]);
 
 const userTable = mysqlTable("test_user", {
-	id: varchar("id", {
-		length: 255
-	}).primaryKey(),
-	username: varchar("username", {
-		length: 255
-	})
-		.notNull()
-		.unique()
+  id: varchar("id", {
+    length: 255,
+  }).primaryKey(),
+  username: varchar("username", {
+    length: 255,
+  })
+    .notNull()
+    .unique(),
 });
 
 const sessionTable = mysqlTable("user_session", {
-	id: varchar("id", {
-		length: 255
-	}).primaryKey(),
-	userId: varchar("user_id", {
-		length: 255
-	})
-		.notNull()
-		.references(() => userTable.id),
-	expiresAt: datetime("expires_at").notNull(),
-	country: varchar("country", {
-		length: 255
-	})
+  id: varchar("id", {
+    length: 255,
+  }).primaryKey(),
+  userId: varchar("user_id", {
+    length: 255,
+  })
+    .notNull()
+    .references(() => userTable.id),
+  expiresAt: datetime("expires_at").notNull(),
+  country: varchar("country", {
+    length: 255,
+  }),
 });
 
 const db = drizzle(connection);
--- workspaces/icedgate/tests/adapter-sqlite.ts
+++ workspaces/icedgate/tests/adapter-sqlite.ts
@@ -1,13 +1,13 @@
 import { testAdapter, databaseUser } from "@lucia-auth/adapter-test";
-import { DrizzleSQLiteAdapter } from "../src/drivers/sqlite.js";
 import sqlite from "better-sqlite3";
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 import { drizzle } from "drizzle-orm/better-sqlite3";
+import { DrizzleSQLiteAdapter } from "../src/drivers/sqlite.js";
 
 const sqliteDB = sqlite(":memory:");
 
 sqliteDB.exec(
-	`CREATE TABLE user (
+  `CREATE TABLE user (
 	id TEXT NOT NULL PRIMARY KEY,
 	username TEXT NOT NULL UNIQUE
 )`
@@ -20,21 +20,21 @@ sqliteDB.exec(
 )`);
 
 sqliteDB
-	.prepare(`INSERT INTO user (id, username) VALUES (?, ?)`)
-	.run(databaseUser.id, databaseUser.attributes.username);
+  .prepare("INSERT INTO user (id, username) VALUES (?, ?)")
+  .run(databaseUser.id, databaseUser.attributes.username);
 
 const userTable = sqliteTable("user", {
-	id: text("id").primaryKey(),
-	username: text("username").notNull().unique()
+  id: text("id").primaryKey(),
+  username: text("username").notNull().unique(),
 });
 
 const sessionTable = sqliteTable("user_session", {
-	id: text("id").primaryKey(),
-	userId: text("user_id")
-		.notNull()
-		.references(() => userTable.id),
-	expiresAt: integer("expires_at").notNull(),
-	country: text("country")
+  id: text("id").primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => userTable.id),
+  expiresAt: integer("expires_at").notNull(),
+  country: text("country"),
 });
 
 const db = drizzle(sqliteDB);
